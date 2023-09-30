121f39fa9ba9636170a1f0f19e11c629e8f90bd2
Felipe <limaflores@gmail.com>
2023-09-29 23:43:55 -0300

    Add functions

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+// const graphqlHTTP = require('express-graphql').graphqlHTTP;
+const { graphqlHTTP } = require('express-graphql');
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const schema = require('./schema');
+
+const app = express();
+const port = 3000;
+
+// require('./orm/Companhia').findAll().then(resposta => {
+//     console.log(resposta);
+// });
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+}));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
